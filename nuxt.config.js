a4d8b46dd43bc93219d3e649ee766f351cf4bbc6
Oliver Noles <olivernoles@xtra.co.nz>
2021-05-15 15:11:49 +1200

    Added deploy scripts

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
+
   target: 'static',
+  router: {
+    base: '/<repository-name>/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -48,5 +51,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+
 }
