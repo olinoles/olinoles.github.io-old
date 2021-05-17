08e1436f7c24f2f68ad5c7cb930c820a55c35a20
Oliver Noles <oliver@noles.nz>
2021-05-18 00:18:51 +1200

    Added mobile menu and submenu

---
+++ components/Header/NavItemMobile.vue
@@ -0,0 +1,54 @@
+<template>
+  <div v-if="hasSubMenu && subMenuItems" class="relative">
+    <!-- Item active: "text-gray-900", Item inactive: "text-gray-500" -->
+    <a href="#" class="text-gray-500 group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" aria-expanded="false" @click="isOpen = !isOpen">
+      <span>{{ label }}</span>
+      <!-- Item active: "text-gray-600", Item inactive: "text-gray-400"
+            -->
+      <ChevronDownIcon class="text-gray-400 ml-2 h-5 w-5 group-hover:text-gray-500" />
+    </a>
+    <div v-if="subMenuItems && isOpen" class="grid gap-y-8 mt-6 ml-4">
+      <div v-for="item in subMenuItems" :key="item.label" :v-key="`subitem-${item.label}`" class="text-base font-medium text-gray-500 hover:text-gray-900">
+        {{ item.label }}
+      </div>
+    </div>
+  </div>
+
+  <nuxt-link v-else :to="path" class="text-base font-medium text-gray-500 hover:text-gray-900">
+    {{ label }}
+  </nuxt-link>
+</template>
+
+<script>
+import { ChevronDownIcon } from '@vue-hero-icons/solid'
+export default {
+  components: {
+    ChevronDownIcon
+  },
+  props: {
+    label: {
+      type: String,
+      required: true
+    },
+    path: {
+      type: [String, Object],
+      required: true
+    },
+    hasSubMenu: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    subMenuItems: {
+      type: Array,
+      required: false,
+      default: null
+    }
+  },
+  data () {
+    return {
+      isOpen: false
+    }
+  }
+}
+</script>
