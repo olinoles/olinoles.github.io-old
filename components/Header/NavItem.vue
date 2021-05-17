61e318bc3eae416ec86b77e774515051b50712fc
Oliver Noles <oliver@noles.nz>
2021-05-17 20:44:04 +1200

    feat(header): create header  - Created header bar - Added header links and components - Added hero icon library - Removed default styling

---
+++ components/Header/NavItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <div v-if="hasSubMenu" class="relative">
+    <!-- Item active: "text-gray-900", Item inactive: "text-gray-500" -->
+    <nuxt-link :to="path" class="text-gray-500 group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" aria-expanded="false">
+      <span>{{ label }}</span>
+      <!-- Item active: "text-gray-600", Item inactive: "text-gray-400"
+            -->
+      <ChevronDownIcon class="text-gray-400 ml-2 h-5 w-5 group-hover:text-gray-500" />
+    </nuxt-link>
+    <!-- <HeaderSubmenu /> -->
+  </div>
+
+  <nuxt-link v-else :to="path" class="text-base font-medium text-gray-500 hover:text-gray-900">
+    {{ label }}
+  </nuxt-link>
+</template>
+
+<script>
+import { ChevronDownIcon } from '@vue-hero-icons/solid'
+export default {
+  components: {
+    ChevronDownIcon
+  },
+  props: {
+    label: {
+      type: String,
+      required: true
+    },
+    path: {
+      type: [String, Object],
+      required: true
+    },
+    hasSubMenu: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  }
+}
+</script>
