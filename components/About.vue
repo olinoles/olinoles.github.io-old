aad8b0162d9ce0b7fe5d82dee3269adc5f2d20f3
Oliver Noles <olivernoles@xtra.co.nz>
2021-05-19 17:44:51 +1200

    Redesigned about page

--- components/About.vue
+++ components/About.vue
@@ -1,37 +1,37 @@
 <template>
-  <div>
-    <div class="bg-white">
-      <!-- Header -->
-      <div class="relative pb-32 bg-gray-800">
-        <div class="absolute inset-0">
-          <div class="absolute bg-gray-800 mix-blend-multiply" aria-hidden="true" />
-        </div>
-        <div class="flex  flex-col md:flex-row sm:flex-col lg:flex-row">
-          <div class="flex-1 inline-block max-w-xl lg:ml-72 py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
-            <h1 class="text-4xl font-extrabold tracking-tight text-white md:text-5xl lg:text-6xl">
-              Hi I'm Oliver
-            </h1>
-            <p class="mt-6 max-w-3xl text-xl text-gray-300">
-              I'm an Auckland-based tech enthusiast experienced in web technologies, creative projects, and UI design.
-            </p>
-          </div>
-          <div
-            class="flex-none justify-center"
-          >
-            <img
-              class="lg:mt-24 h-60 w-60 ml-20 mx-auto rounded-full"
-              src="~/assets/profile.jpg"
-              alt=""
-            >
-          </div>
-        </div>
+  <div class="pb-4 lg:pb-10 bg-darkblue">
+    <div class="flex justify-center flex-col md:flex-row lg:mx-72 lg:flex-row">
+      <div class="inline-block max-w-xl lg:py-20 px-4 py-8 sm:px-6 lg:px-8">
+        <h1 class="text-4xl font-extrabold tracking-tight text-white md:text-5xl lg:text-6xl">
+          Hi, I'm Oliver
+        </h1>
+        <p class="mt-6 max-w-3xl text-xl text-gray-300">
+          I'm an Auckland-based tech enthusiast experienced in web technologies, creative projects, and UI design.
+        </p><p class="mt-6 max-w-3xl text-xl text-gray-300">
+          I am most interested in full-stack development and enjoy working with both front-end and back-end tools. Outside of webdev, I enjoy hobbies that involve robotics and electronics, including 3D printing, building FPV Drones and CAD design.
+        </p>
       </div>
+      <img
+        class="mt-8 lg:mt-24 lg:h-80 lg:w-80 w-52 h-52 mx-auto lg:mx-20  rounded-full ring-4 ring-gray-600 ring-opacity-50"
+        src="~/assets/profile.jpg"
+        alt=""
+      >
     </div>
+    <ChevronDownIcon class="text-white text-xl mx-auto h-8 w-8 mt-8" />
   </div>
 </template>
 
 <script>
+import { ChevronDownIcon } from '@vue-hero-icons/solid'
 export default {
-
+  components: {
+    ChevronDownIcon
+  }
 }
 </script>
+
+<style scoped>
+.bg-darkblue {
+  background-color: #14213D;
+}
+</style>
