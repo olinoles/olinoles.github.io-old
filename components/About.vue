85b60ccfbcd04b352b1a8705f7f3874190949f30
Oliver Noles <olivernoles@xtra.co.nz>
2021-05-20 22:07:03 +1200

    Fix anchor link

--- components/About.vue
+++ components/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="pb-4 lg:pb-10 bg-darkblue">
+  <div id="about" class="pb-4 lg:pb-10 bg-darkblue">
     <div class="mt-4 right-4 space-x-3 hidden sm:inline absolute">
       <a href="https://github.com/olinoles" target="_blank"><img src="/portfolio/icons/github.svg" class="inline-block h-8 w-auto hover:scale-110 transform duration-75 cursor-pointer opacity-70 hover:opacity-100"></a>
       <a href="https://www.linkedin.com/in/oliver-noles-2390a8178/" target="_blank"><img src="/portfolio/icons/linkedin.svg" class="inline-block h-8 w-auto hover:scale-110 transform duration-75 cursor-pointer opacity-70 hover:opacity-100"></a>
