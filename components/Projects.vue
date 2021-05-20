df6c5bde2098c9f857949b29c3c56d963a21e70c
Oliver Noles <oliver@noles.nz>
2021-05-20 15:48:15 +1200

    Built project summaries and added content

--- components/Projects.vue
+++ components/Projects.vue
@@ -1,15 +1,37 @@
 <template>
-  <div id="skills" class="pb-32 med-blue lg:pt-20 bg-image mx-auto p-5 w-full">
+  <div id="skills" class="pb-32 bg-light-blue lg:pt-20 bg-image mx-auto p-5 w-full">
     <div class="mx-auto my-5 max-w-5xl relative pl-6">
-      <h2 class="title mb-4 text-2xl lg:text-4xl">
+      <h2 class="title mb-10 text-2xl lg:text-4xl">
         Featured Projects
       </h2>
+      <ProjectSummary
+        title="ASColour Colourmatic Critic"
+        cta="Watch Video"
+        cta-link="https://vimeo.com/312834441"
+        description="This creative project for ASColour used a camera and visuals to recommend t-shirts to customers. The display was featured outside the Customs Street store in Auckland CBD."
+        link="ascolour"
+        image="ascolour.png"
+      />
+      <ProjectSummary
+        title="KiwiQuads 3D Drone Customiser"
+        cta="Live Site"
+        cta-link="https://kiwiquads.co.nz/customiser"
+        description="A 3D drone customiser tool built for KiwiQuads allows real-time customisation of a drone to visualise different components and colours.
+Built using Babylon.js framework and React."
+        link="customiser"
+        image="customiser.jpg"
+      />
+      <ProjectSummary title="KiwiQuads Order Dispatch System" description="KiwiQuads needed a dispatching system to handle a large quantity of orders efficiently. I built this web application to combine order picking with NZ Posts API for shipping." link="dispatch" image="dispatch.jpg" />
     </div>
   </div>
 </template>
 <script>
-export default {
+import ProjectSummary from './Projects/ProjectSummary.vue'
 
+export default {
+  components: {
+    ProjectSummary
+  }
 }
 </script>
 
---
+++ components/Projects/ProjectSummary.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="flex justify-start">
+    <nuxt-link :to="`projects/` + link">
+      <div class="flex flex-row bg-white w-auto hover:shadow-md shadow-sm rounded-lg overflow-hidden mb-12 cursor-pointer">
+        <img :src="`/portfolio/projects/${image}`" class="h-auto w-72">
+        <div class="flex flex-col ml-10">
+          <div class="text-xl my-4">
+            {{ title }}
+          </div>
+          <div class="text-sm mb-4 mr-10 max-w-prose">
+            {{ description }}
+          </div>
+          <div v-if="cta" class="inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mb-4 mr-auto">
+            <a target="_blank" :href="ctaLink">
+              {{ cta }}
+            </a>
+          </div>
+        </div>
+      </div>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    link: {
+      type: String,
+      required: true
+    },
+    cta: {
+      type: String,
+      required: false,
+      default: null
+    },
+    ctaLink: {
+      type: String,
+      required: false,
+      default: null
+    },
+    image: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
