fa7d70b051919c493cce74f3cb06c936a31ec2e1
Oliver Noles <olivernoles@xtra.co.nz>
2021-05-25 21:56:02 +1200

    Rewrote interest description

--- components/Interests/FPVRacingDrones.vue
+++ components/Interests/FPVRacingDrones.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col sm:flex-row">
     <p class="mr-5 mb-5">
-      In 2017 I purchased my first racing drone - A 'Wizard X220', capable of flying at up to 130kmh! This lead to an obsession with me opening my own dedicated racing drone parts business in 2018. KiwiQuads is now NZ's largest FPV drone specialist store.<br><br>Racing Drone technology moves extremely quickly. New products are released every month which allows the drones to fly faster and with more precision for racing.
+      In 2017 I purchased my first racing drone - A 'Wizard X220', capable of flying at up to 130kmh! This became an obsession which lead to me opening my own dedicated racing drone parts business in 2018. KiwiQuads is now NZ's largest FPV drone specialist store.<br><br>Racing Drone technology moves extremely quickly. New products are released every month which allows the drones to fly faster and with more precision for racing.
     </p>
     <img class="w-64 h-auto shadow-lg rounded-md" src="~/static/images/fpvdrone.jpg">
   </div>
