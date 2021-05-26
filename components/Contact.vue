0c10957db802b9179a7b39c499399ef8588ef9b4
Oliver Noles <olivernoles@xtra.co.nz>
2021-05-26 15:13:24 +1200

    Added contact section

---
+++ components/Contact.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="contact" class="pb-4 lg:pb-10 bg-darkblue">
+    <div class="mt-4 right-4 space-x-3 hidden sm:inline absolute" />
+    Contact Me
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.bg-darkblue {
+  background-color: #14213D;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,11 +4,13 @@
     <skills />
     <projects />
     <interests />
+    <contact />
   </div>
 </template>
 
 <script>
 import About from '~/components/About.vue'
+import Contact from '~/components/Contact.vue'
 import Projects from '~/components/Projects.vue'
-export default { components: { About, Projects } }
+export default { components: { About, Projects, Contact } }
 </script>
