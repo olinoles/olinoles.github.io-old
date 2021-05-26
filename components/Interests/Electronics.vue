04c587720570793ace8849c054653f624f57f2ed
Oliver Noles <olivernoles@xtra.co.nz>
2021-05-26 21:03:07 +1200

    Fix for WEBM on mobile browsers

--- components/Interests/Electronics.vue
+++ components/Interests/Electronics.vue
@@ -3,7 +3,7 @@
     <p class="mr-5 mb-5">
       Since I was young I've always enjoyed pulling apart devices and seeing how they worked. This prompted me to pursue a career an as Electronics Technician in the NZ Navy, where I worked and gained qualifications in the Electrical Engineering field. I still regularly tinker with electronics such as Arduinos and Raspberry Pis, now combined with 3D printing I can build some really cool projects! <br><br>Here is an Arduino controlled 3D turret I built using stepper motors in action. This project is clearly still a WIP!
     </p>
-    <video autoplay loop class="w-60 rounded-md">
+    <video playsinline autoplay loop class="w-60 rounded-md">
       <source
         src="~/static/images/turret.webm"
         type="video/webm"
