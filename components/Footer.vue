8feab725b47a7eecc74a46efb57346752442937a
Oliver Noles <oliver@noles.nz>
2021-05-26 18:10:51 +1200

    Added footer

---
+++ components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-gray-900 p-5 text-center text-gray-600">
+    © Oliver Noles {{ new Date().getFullYear() }}<br>
+    I built this website myself! You can view the <a class="text-gray-500 hover:underline" el="noreferrer" target="_blank" href="https://github.com/olinoles/olinoles.github.io">source here</a> on GitHub.
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
