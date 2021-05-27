2d723eb60c86e2555c139c071843208a55691206
Oliver Noles <oliver@noles.nz>
2021-05-27 12:22:17 +1200

    Added personal logo

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -4,7 +4,7 @@
       <div class="flex justify-between items-center py-6 md:justify-start md:space-x-10">
         <div class="flex justify-start lg:w-0 lg:flex-1">
           <a href="#">
-            <img id="logo" class="h-8 w-auto sm:h-10" src="" alt="Oliver Noles">
+            <img id="logo" class="h-8 w-auto sm:h-10" src="~/static/icons/oliver.png" alt="Oliver Noles">
           </a>
         </div>
         <div class="-mr-2 -my-2 md:hidden">
@@ -37,7 +37,7 @@
           <div class="pt-5 pb-6 px-5">
             <div class="flex items-center justify-between">
               <div>
-                <img id="logo" class="h-8 w-auto" src="" alt="Oliver Noles">
+                <img id="logo" class="h-8 w-auto" src="~/static/icons/oliver.png" alt="Oliver Noles">
               </div>
               <div class="-mr-2">
                 <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" @click="isMobileMenuOpen = false">
