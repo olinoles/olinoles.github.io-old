fbf7ecce1ca1d5ccc4ef64a6d04b43dfa30591c6
Oliver Noles <oliver@noles.nz>
2021-05-31 23:01:35 +1200

    Built custom 404 page

---
+++ layouts/error.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="max-w-5xl mx-auto mt-32 sm:mt-52 px-10">
+    <p class="text-5xl text-center">
+      There’s nothing here :(
+    </p>
+    <h1 class="text-3xl mt-10 text-center">
+      404 - Page not found
+    </h1>
+    <p class="text-2xl sm:text-3xl mt-32 mb-32 text-center text-gray-400">
+      Was it my fault? Please <a class="underline" href="mailto:oliver@noles.nz?subject=You dun goofed!">let me know!</a>
+    </p>
+  </div>
+</template>
