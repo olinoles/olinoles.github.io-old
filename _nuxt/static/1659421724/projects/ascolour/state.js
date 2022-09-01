181fb38c972b4f29bfc323682c130f54b3d7cf5a
olinoles <olinoles@users.noreply.github.com>
2022-09-01 02:45:56 +0000

    deploy: 8dd8ab84e2e4651bbf8cdedf1512d909715046e0

--- _nuxt/static/1659421724/projects/ascolour/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1659421724",layout:"project",error:a,serverRendered:true,routePath:"\u002Fprojects\u002Fascolour",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
